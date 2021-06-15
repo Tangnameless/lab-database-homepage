0012fb4bcbca503c88a596f7fc282bdc0e5b7240
Tangnameless <tang0079@foxmail.com>
2021-06-15 16:07:56 +0800

    Update Register.vue
    出现了一个bug

--- entrance/src/views/Register.vue
+++ entrance/src/views/Register.vue
@@ -3,8 +3,43 @@
     <el-container>
       <el-main>
         <div>
-          <el-card header="用户注册" class="register-card">
+          <el-card class="register-card">
+            <div slot="header" class="clearfix">
+              <span>用户注册</span>
+              <el-link
+                class="login-link"
+                @click="handleClick"
+                style="float: right; padding: 3px 0"
+                >返回登录页面</el-link
+              >
+            </div>
+            <el-steps :active="active" finish-status="success" align-center>
+              <el-step title="邮箱验证"></el-step>
+              <el-step title="注册信息"></el-step>
+            </el-steps>
+            <!-- 邮箱验证表单 -->
             <el-form
+              v-if="active == 0"
+              status-icon
+              :model="ruleForm"
+              :rules="rules"
+              ref="ruleEmail"
+              :inline="true"
+              label-width="100px"
+              class="demo-ruleForm"
+            >
+              <el-form-item prop="email" label="邮箱">
+                <el-input v-model="ruleForm.email"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="validEmail('ruleEmail')"
+                  >验证邮箱</el-button
+                >
+              </el-form-item>
+            </el-form>
+            <!-- 注册信息表单 -->
+            <el-form
+              v-if="active == 1"
               status-icon
               :model="ruleForm"
               :rules="rules"
@@ -16,14 +51,10 @@
               <el-form-item label="用户名称" prop="username">
                 <el-input v-model="ruleForm.username"></el-input>
               </el-form-item>
-              <el-form-item prop="email" label="邮箱">
-                <el-input v-model="ruleForm.email"></el-input>
-              </el-form-item>
-
-              <el-form-item label="密码" prop="password">
+              <el-form-item label="密码" prop="pass">
                 <el-input
                   type="password"
-                  v-model="ruleForm.password"
+                  v-model="ruleForm.pass"
                   autocomplete="off"
                 ></el-input>
               </el-form-item>
@@ -48,27 +79,38 @@
               </el-form-item>
               <el-form-item>
                 <el-button type="primary" @click="submitForm('ruleForm')"
-                  >立即创建</el-button
+                  >立即注册</el-button
                 >
                 <el-button @click="resetForm('ruleForm')">重置</el-button>
-
-                <el-link class="login-link" @click="handleClick"
-                  >返回登录页面</el-link
-                >
               </el-form-item>
             </el-form>
+            <el-divider></el-divider>
+            <el-button v-if="active == 1" style="margin-top: 12px" @click="back"
+              >上一步</el-button
+            >
+            <el-button v-if="active == 0" style="margin-top: 12px" @click="next"
+              >下一步</el-button
+            >
           </el-card>
-        </div></el-main
-      >
+        </div>
+      </el-main>
       <el-footer>北京邮电大学电子工程学院通信与网络研究中心（2021）</el-footer>
     </el-container>
   </div>
 </template>
 
 <script>
+// 注册流程
+// step1：
+// 进行邮箱验证，可以跳过，然后在个人中心完成邮箱验证
+// step2:
+// 填写注册信息，并提交
+// step3:
+// 返回登陆页面
+
 export default {
   data() {
-    var validatePass = (rule, value, callback) => {
+    var validatePass1 = (rule, value, callback) => {
       if (value === "") {
         callback(new Error("请输入密码"));
       } else {
@@ -78,20 +120,23 @@ export default {
         callback();
       }
     };
+
     var validatePass2 = (rule, value, callback) => {
       if (value === "") {
         callback(new Error("请再次输入密码"));
-      } else if (value !== this.ruleForm.password) {
+      } else if (value !== this.ruleForm.pass) {
         callback(new Error("两次输入密码不一致!"));
       } else {
         callback();
       }
     };
+
     return {
+      active: 0, // 用来控制处于第几个step和相应的表单显示
       ruleForm: {
         username: "",
         email: "",
-        password: "",
+        pass: "",
         checkPass: "",
         role: "",
       },
@@ -117,7 +162,7 @@ export default {
             trigger: ["blur", "change"],
           },
         ],
-        password: [{ validator: validatePass, trigger: "blur" }],
+        pass: [{ validator: validatePass1, trigger: "blur" }],
         checkPass: [{ validator: validatePass2, trigger: "blur" }],
         role: [
           { required: true, message: "请选择用户角色", trigger: "change" },
@@ -126,6 +171,21 @@ export default {
     };
   },
   methods: {
+    validEmail(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // 前端成功验证数据格式，发送后端注册接口
+          alert("submit!");
+          this.sendEmail();
+        } else {
+          this.$message({
+            type: "warning",
+            message: "无效的邮箱信息",
+          });
+          return false;
+        }
+      });
+    },
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
@@ -144,17 +204,19 @@ export default {
     resetForm(formName) {
       this.$refs[formName].resetFields();
     },
+    // 根据ZAS接口使用, 验证邮箱
+    async sendEmail() {},
     async register() {
       // 根据ZAS接口使用，用户名与密码, 与signupID存放在headers中
       let info = {
         username: this.ruleForm.username,
-        password: this.ruleForm.password,
+        password: this.ruleForm.pass,
         signupId: this.ruleForm.role,
       };
       await this.$http.post("/auth/signup", undefined, { headers: info });
       this.$message({
         type: "success",
-        message: "登陆成功",
+        message: "注册成功！",
       });
       // 跳转回登录页面
       this.$router.push("/login");
@@ -163,6 +225,15 @@ export default {
     // 处理el-link中的点击，跳转回登录页面
     async handleClick() {
       this.$router.push("/login");
+    },
+
+    // 控制step前进
+    next() {
+      if (this.active++ >= 2) this.active = 2;
+    },
+    // 控制step后退
+    back() {
+      if (this.active-- <= 0) this.active = 0;
     },
   },
 };
@@ -193,6 +264,10 @@ export default {
 .el-container {
   /*统一设置高度为100%*/
   height: 100%;
+}
+
+.el-form {
+  margin-top: 20px;
 }
 /* 表单输入框label的样式修改 */
 .el-form-item__label {
